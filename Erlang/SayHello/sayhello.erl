b23ee6cc309fcbe853c782ba469a560c8bdf91f1
Wolfgang Loder <wolfgang.loder@googlemail.com>
2015-08-13 09:34:08 +0300

    SayHello Example updated
    Erlang and Elixir

--- Erlang/SayHello/sayhello.erl
+++ Erlang/SayHello/sayhello.erl
@@ -4,6 +4,11 @@
 %%% This module is a basic example of
 %%%    (1) sending messages between processes and
 %%%    (2) distribute processes on different machines.
+%%% Usage:
+%%% Machine 1: erl -sname precipient -setcookie scookie
+%%%            sayhello:start_recipient().
+%%% Machine 2: erl -sname psayhello -setcookie scookie
+%%%            sayhello:start_sayhello("precipient@wlmb").
 %%% @end
 %%%-------------------------------------------------------------------
 -module(sayhello).
@@ -13,11 +18,10 @@
 -export([start/0, recipient/0, say_hello/2, say_hello/3, start_recipient/0, start_sayhello/1]).
 
 %% methods for distributed processes on more than one machine
-say_hello(What, 0, Node) ->
+say_hello(_, 0, Node) ->
   io:format("Process arity 3 'say_hello' finished~n", []),
   {precipient, Node} ! finished;
 say_hello(What, Times, Node) ->
-%%   Node ! {log, self()},
   io:format("3: ~p~n", [What]),
   say_hello(What, Times - 1, Node).
 
@@ -25,10 +29,11 @@ start_recipient() ->
   register(precipient, spawn(sayhello, recipient, [])).
 
 start_sayhello(InitNode) ->
-  spawn(sayhello, say_hello, [hi, 2, InitNode]).
+  spawn(sayhello, say_hello, [hi, 2, InitNode]),
+  {precipient, InitNode} ! log.
 
 %% methods for processes on one machine
-say_hello(What, 0) ->
+say_hello(_, 0) ->
   io:format("Process arity 2 'say_hello' finished~n", []),
   precipient ! finished;
 say_hello(What, Times) ->
@@ -40,7 +45,7 @@ recipient() ->
   receive
     finished ->
       io:format("Recipient process finished~n", []);
-    {log, _} ->
+    log ->
       io:format("Recipient received log message~n", []),
       recipient();
     _ ->
@@ -50,6 +55,5 @@ recipient() ->
 
 start() ->
   register(precipient, spawn(sayhello, recipient, [])),
-  precipient ! stillnomessagedefined,
+  precipient ! nomessagedefined,
   spawn(sayhello, say_hello, [hi, 4]).
-
